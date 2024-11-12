ba65b2de4122fdf29f200fa60044aaf82671803a
drewsawicki <drewsawicki@gmail.com>
2024-11-12 11:39:59 -0500

    Make author optional argument

--- main.py
+++ main.py
@@ -11,8 +11,7 @@ subparsers = parser.add_subparsers(dest="command", help="Available commands")
 # Subparser for adding a book
 add_book_parser = subparsers.add_parser("add", help="Add a new book to the tracker")
 add_book_parser.add_argument("--title", required=True, help="Title of the book")
-add_book_parser.add_argument("--author", required=True, help="Author of the book")
-add_book_parser.add_argument("--pages", required=True, help="Total number of pages")
+add_book_parser.add_argument("--author", required=False, help="Author of the book")
 
 # Subparser for updating progress
 update_progress_parser = subparsers.add_parser("update", help="Update progress on a book")
@@ -28,10 +27,10 @@ book_details_parser.add_argument("--title", required=True, help="Title of the bo
 
 args = parser.parse_args()
 if args.command == "add":
-    if args.title and args.author and args.pages:
-        tracker.add_book(args.title, args.author, int(args.pages))
+    if args.title:
+        tracker.add_book(args.title, args.author)
     else:
-        print("Please provide title, author, and total number of pages for the book.")
+        print("Title of the book is required.")
 elif args.command == "update":
     if args.title and args.page:
         tracker.update_book_progress(args.title, int(args.page))
@@ -43,6 +42,6 @@ elif args.command == "details":
     if args.title:
         tracker.display_book_details(args.title)
     else:
-        print("Please provide title of the book.")
+        print("Title of the book is required.")
 else:
     parser.print_help()
